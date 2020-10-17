cb86154554694d4f790f53b6b6a8f52e82b405f2
Ruben Chalapud <ruben.chalapud@uao.edu.co>
2020-10-17 18:31:07 -0500

    Corregimos estilos y agregamos funcion a boton de Autoevaluacion de Activity InicioDocente y agregamos activity AutoevaluacionDocente con estilos y componentes.

--- app/src/main/java/com/example/uaoremoto/InicioDocente.java
+++ app/src/main/java/com/example/uaoremoto/InicioDocente.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class InicioDocente extends AppCompatActivity {
@@ -15,12 +17,18 @@ public class InicioDocente extends AppCompatActivity {
     TextView txtUser;
     TextView txtCorreo;
 
+    Button btnRealizarAutoE;
+
+    //Inicializar menu
     DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio_docente);
+
+        //Boton para evaluar covid
+        btnRealizarAutoE = (Button) findViewById(R.id.btnRealizarEvaluacion);
 
         txtUser =(TextView)findViewById(R.id.textViewNombreD);
         String user = getIntent().getStringExtra("names");
@@ -30,15 +38,25 @@ public class InicioDocente extends AppCompatActivity {
         String email = getIntent().getStringExtra("email");
         txtCorreo.setText(email);
 
+        //Menu
         drawerLayout = findViewById(R.id.drawer_layout);
+
+        //Ir a evaluar covid
+        btnRealizarAutoE.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(InicioDocente.this, AutoevaluacionDocente.class);
+                startActivity(i);
+            }
+        });
     }
 
+    //Metodos para Menu
     public void ClickMenu(View view){
         openDrawer(drawerLayout);
     }
 
     private static void openDrawer(DrawerLayout drawerLayout){
         drawerLayout.openDrawer(GravityCompat.START);
-
     }
 }
